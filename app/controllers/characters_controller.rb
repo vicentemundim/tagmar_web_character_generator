b570f233ed6c613c2f212bc80c44811f84695edf
VIcente Mundim <vicente@roxo1.(none)>
2009-02-04 19:29:21 -0200

    Prevent users to update other users characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,6 +2,14 @@ class CharactersController < ApplicationController
   resource_controller
 
   before_filter :login_required
+  before_filter :prevent_user_to_update_other_users_characters, :only => [:new, :create, :edit, :update]
 
   belongs_to :user
+
+  def prevent_user_to_update_other_users_characters
+    unless current_user.id.to_s == params[:user_id]
+      flash[:error] = "Cannot update other users characters."
+      redirect_to('/')
+    end
+  end
 end
