6cc99c78b2635cc5fa30516f78118a56d51fca29
VIcente Mundim <vicente@roxo1.(none)>
2009-02-06 19:23:58 -0200

    Adding gods rules class

---
+++ app/models/rules/gods.rb
@@ -0,0 +1,21 @@
+class Rules::Gods
+  DESCRIPTIONS = {
+    'sevides'   => 'Agricultura, fertilidade',
+    'quiris'    => 'Plantio',
+    'liris'     => 'Colheita',
+    'ganis'     => 'Água, mar',
+    'blator'    => 'Guerra, conflito',
+    'crisagom'  => 'Honra, estratégia, bravura',
+    'crezir'    => 'Fúria, matança, prazer da luta',
+    'maira mon' => 'Natureza em apescto mineral',
+    'maira vet' => 'Natureza em aspecto vegetal',
+    'maira nil' => 'Natureza em aspecto animal',
+    'selimom'   => 'Paz, amor',
+    'lena'      => 'Prazer erótico e prazer no trabalho',
+    'plandis'   => 'Paixão cega',
+    'cambu'     => 'Diplomacia, comércio, interrelações',
+    'cruine'    => 'Morte, protetor das almas, reencarnação',
+    'palier'    => 'Conhecimento, magia',
+    'parom'     => 'Artíficies',
+  }
+end
