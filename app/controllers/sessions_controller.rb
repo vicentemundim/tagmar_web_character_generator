8138b7885368e0d4713fbb3fe2c0ce12f5effe01
VIcente Mundim <vicente@roxo1.(none)>
2009-02-07 15:36:21 -0200

    Adding styles to login and signup forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
       @login       = params[:login]
@@ -27,14 +26,13 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "You have been logged out."
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = "Couldn't log you in as '#{params[:login]}'"
+    flash[:error] = "Usuário ou senha inválidos."
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,29 @@
 class UsersController < ResourceController::Base
   before_filter :prevent_user_update_other_users_data, :only => [:edit, :update]
 
+  [new_action, edit].each do |action|
+    action.before do
+      @user.password = @user.password_confirmation = nil
+    end
+  end
+
   create do
     before { logout_keeping_session! }
     after { self.current_user = @user }
 
     success do
-      flash "Thanks for signing up! Please, login now to access your account."
+      flash "Obrigado por registrar-se!"
       wants.html { redirect_back_or_default('/') }
     end
 
-    failure.flash "Could not create your account."
+    failure.flash "Corrija os erros abaixo."
   end
 
   update.wants.html { redirect_to('/') }
 
   def prevent_user_update_other_users_data
     unless current_user.id.to_s == params[:id]
-      flash[:error] = "Cannot update other users data."
+      flash[:error] = "Não é possível editar dados de outros usuários."
       redirect_to('/')
     end
   end
