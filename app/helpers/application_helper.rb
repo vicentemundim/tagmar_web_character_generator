313795d7d2a01e5b2684e2bc1a9038eb3788d323
VIcente Mundim <vicente@roxo1.(none)>
2009-02-07 18:14:18 -0200

    Adding style to character sheet page (show)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,13 @@ module ApplicationHelper
       "http://www.gravatar.com/avatar/#{MD5.md5(current_user.email)}?s=#{width}&default=#{default_avatar_url}"
     end
   end
+
+  def toggler_for(target_id, toggler_id, start_hidden = false)
+    toggle_text = "$('#{target_id}').visible() ? $('#{toggler_id}').innerHTML = '[esconder]' : $('#{toggler_id}').innerHTML = '[exibir]'"
+    if start_hidden
+      link_to_function '[exibir]', "$('#{target_id}').toggle(); #{toggle_text}", :id => toggler_id
+    else
+      link_to_function '[esconder]', "$('#{target_id}').toggle(); #{toggle_text}", :id => toggler_id
+    end
+  end
 end
